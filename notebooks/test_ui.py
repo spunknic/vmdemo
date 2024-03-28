f1256186cf4b63877b0847a73304436b08857f7a
Juan Silva <spunknic@gmail.com>
2024-03-28 10:11:57 -0400

    sdc
    sd

--- notebooks/test_ui.py
+++ notebooks/test_ui.py
@@ -10,6 +10,7 @@ if uploaded_file is not None:
     df = dataframe
     df                = pd.DataFrame(df, columns=['ID', 'Timestamp', 'Count', 'RSSI_min', 'RSSI_max', 'RSSI_avr', 'RSSI_med', 'Longitude', 'Latitude', 'Flag'])
     n_df              = df.drop(columns=['RSSI_min', 'RSSI_max', 'RSSI_med', 'Flag'])
+    st.write(df)
     st.write(n_df)
     #format ID
     n_df['ID']          = n_df['ID'].apply(lambda x: x[1:])
