fc7c9950cf85245b4d6018903e15a700eae46ea0
Juan Silva <spunknic@gmail.com>
2024-03-28 10:41:14 -0400

    info
    dc

--- notebooks/test_ui.py
+++ notebooks/test_ui.py
@@ -10,15 +10,17 @@ if uploaded_file is not None:
     df.columns = ['ID', 'Timestamp', 'Count', 'RSSI_min', 'RSSI_max', 'RSSI_avr', 'RSSI_med', 'Longitude', 'Latitude', 'Flag']
     n_df              = df.drop(columns=['RSSI_min', 'RSSI_max', 'RSSI_med', 'Flag'])
     #format ID
-    n_df['ID']          = n_df['ID'].apply(lambda x: x[1:])
+    n_df['ID']          = n_df['ID'].apply(lambda x: x.replace('[', ''))
     #format count
     n_df['Count'] = pd.to_numeric(n_df['Count'])
     #format RSSI_avr
     n_df['RSSI_avr'] = pd.to_numeric(n_df['RSSI_avr'])
     #format Longitude
     n_df['Longitude'] = n_df['Longitude'].apply(lambda x: float(x.strip().strip('"')))
+    n_df['Longitude'] = n_df['Longitude'].apply(lambda x: float(x))
     #format Latitude
     n_df['Latitude'] = n_df['Latitude'].apply(lambda x: float(x.strip().strip('"')))
+    n_df['Longitude'] = n_df['Longitude'].apply(lambda x: float(x))
     #format Flag 
     n_df = df.drop(columns=['RSSI_min', 'RSSI_max', 'RSSI_med', 'Flag'])
     #Others
