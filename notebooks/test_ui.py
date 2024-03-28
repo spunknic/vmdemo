ae65d4cce6150a60f492156c893efeeedb9c8bea
Juan Silva <spunknic@gmail.com>
2024-03-28 09:56:19 -0400

    new test
    new test

--- notebooks/test_ui.py
+++ notebooks/test_ui.py
@@ -4,18 +4,12 @@ from io import StringIO
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
 
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
-
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
     dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+    
+    df = dataframe
+    n_df = df.drop(columns=['RSSI_min', 'RSSI_max', 'RSSI_med', 'Flag'])
+    n_df = n_df.drop(n_df[(n_df['Longitude'] == 10) & (n_df['Latitude'] == 10)].index)
+    n_df['Latitude'] = pd.to_numeric(n_df['Latitude'])
+    n_df['Longitude'] = pd.to_numeric(n_df['Longitude'])
+    st.write(n_df)
